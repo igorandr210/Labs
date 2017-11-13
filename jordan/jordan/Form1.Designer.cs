dac8899e9d31c560dade1f537ac91883125978a9
Igor <Igor@DESKTOP-SJCHKJ5>
2017-11-13 13:46:01 +0200

    kfk

---
+++ jordan/jordan/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace jordan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 187);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(260, 62);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Рахувати!";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 43);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(214, 138);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Введіть через пробіл матрицю рівняння. \"Enter\" для нового рядка. ";
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(227, 43);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(45, 138);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "Вільні члени через \"Enter\"";
+            this.textBox2.Enter += new System.EventHandler(this.textBox2_Enter);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 12);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(144, 20);
+            this.textBox3.TabIndex = 1;
+            this.textBox3.Text = "Введіть кількість змінних";
+            this.textBox3.Enter += new System.EventHandler(this.textBox3_Enter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Жордан Гаус Компані";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ jordan/jordan/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace jordan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = Convert.ToInt32(textBox3.Text);
+            string[] s = textBox1.Text.Split('\n');
+            
+            double[,] A = new double[s.Length,n];
+            int i = 0;
+            int j = 0;
+            foreach (var c in s)
+            {
+                var temp=c.Split(' ');
+                foreach(var t in temp)
+                {
+                    A[i, j] = Convert.ToDouble(t);
+                    j++;
+                }
+                j = 0;
+                i++;
+            }
+            
+            string[] g = textBox2.Text.Split('\n');
+            double[] B = new double[g.Length];
+            i = 0;
+            foreach (var c in g)
+            {
+                B[i] = Convert.ToDouble(c);
+                i++;
+            }
+            var f =Jordan(A, B, n);
+            string Stroka = "";
+            i = 0;
+            foreach (var t in f)
+            {
+                i++;
+                Stroka +=String.Format("X[{0}] = ",i)+ t + "\n";
+            }
+            MessageBox.Show(Stroka);
+        }
+        private double[] Jordan(double[,] A,double[] B,int n)
+        {
+            for (int k = 0; k < n; k++)
+            {
+                double d = A[k, k];
+                A[k, k] = 1.0;
+                for (int j = k + 1; j < n; j++)
+                {
+                    A[k, j] = A[k, j] / d;
+                }
+                B[k] = B[k] / d;
+                for (int i = 0; i < n; i++)
+                {
+                    if (i != k)
+                    {
+                        double S = A[i, k];
+                        A[i, k] = 0;
+                        for (int j = k + 1; j < n; j++)
+                        {
+                            A[i, j] = A[i, j] - S * A[k, j];
+                        }
+                        B[i] = B[i] - S * B[k];
+                    }
+                }
+            }
+            return B;
+        }
+        bool tx1 = true;
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            if (tx1)
+            {
+                textBox1.Text = "";
+                tx1 = false;
+            }
+        }
+        bool tx2 = true;
+        private void textBox2_Enter(object sender, EventArgs e)
+        {
+            if (tx2)
+            {
+                textBox2.Text = "";
+                tx2 = false;
+            }
+        }
+        bool tx3 = true;
+        private void textBox3_Enter(object sender, EventArgs e)
+        {
+            if (tx3)
+            {
+                textBox3.Text = "";
+                tx3 = false;
+            }
+        }
+    }
+}
